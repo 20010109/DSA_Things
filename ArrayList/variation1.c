7e6f8867a470a521a9c17d194f0dfabbfd0ba036
John Lorenz Codilla <20010109@usc.edu.ph>
2025-08-20 11:59:57 +0800

    variationList1

---
+++ ArrayList/variation1.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define size 10
+
+typedef struct{
+    int elem[size];
+    int count;
+}List;
+
+List initialize ();
+List insertPos(List L, int data, int position);
+List deletePos(List L, int position);
+int locate (List L, int data);
+List insertSorted(List L, int data);
+void display(List L);
+
+int main(){
+    List L = initialize();
+    int num, position;
+
+    // printf("Enter integer: ");
+    // scanf("%d", &num);
+    // printf("Enter position: ");
+    // scanf("%d", &position);
+    // L = insertPos(L, num, position);
+
+    L = insertPos(L, 5, 0);
+    L = insertPos(L, 10, 1);
+    L = insertPos(L, 15, 2);
+    display(L);
+
+    printf("\nAfter Deletion\n");
+    L = deletePos(L, 1);
+    display(L);
+
+    printf("\nLocate number\n");
+    int found = locate(L, 20);
+    if(found != -1){
+        printf("Data found on index %d\n", found);
+    } else {
+        printf("Data not found!\n");
+    }
+    display(L);
+
+    printf("\nInsert Sorted\n");
+    L = insertSorted(L, 30);
+    display(L);
+}
+
+void display(List L){
+    printf("List:\n");
+    for(int i = 0; i < size; i++){
+        printf("%d ", L.elem[i]);
+    }
+    printf("\nCount = %d", L.count);
+}
+
+List initialize (){
+    List L;
+    for(int i = 0; i < size; i++){
+        L.elem[i] = -1;
+    }
+    L.count = 0;
+    return L;
+}
+
+List insertPos(List L, int data, int position){
+    if(L.count < size){
+        if(L.elem[position] == -1){
+            L.count++;
+        }
+        L.elem[position] = data;
+    }
+    return L;
+}
+
+List deletePos(List L, int position){
+    L.elem[position] = -1;
+    L.count--;
+    return L;
+}
+
+int locate (List L, int data){
+    printf("Looking for %d: ", data);
+    for(int i = 0; i < size; i++){
+        if(L.elem[i] == data && data != -1){
+            return i;
+        }
+    }
+    return -1;
+}
+
+List insertSorted(List L, int data){
+    for(int i = 1; i < size; i++){
+        int key = data;
+        int j = i-1;
+        while(j >= 0 && L.elem[j] > key){
+            L.elem[j+1] = L.elem[j];
+            j = j - 1;
+        }
+        L.elem[j+1] = key;
+    }
+    return L;
+}
