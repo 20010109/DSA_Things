2e6a2f9a5c0ad638c35bd4c40acad8a24a703d00
John Lorenz Codilla <160460442+20010109@users.noreply.github.com>
2025-11-26 19:22:58 +0800

    Refactor adjacency list and add BFS implementation

--- Topics/Graphs/AdjList.c
+++ Topics/Graphs/AdjList.c
@@ -4,14 +4,20 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define MAX 9
+#define MAX 5
 #define VISITED -1
-#define UNVISITED 10
+#define UNVISITED 0
 
 typedef struct node{
     int elem;
     struct node* link;
 }*ptr, Node;
+
+typedef struct Queue{
+    int q[MAX];
+    int front;
+    int rear;
+}Queue;
 
 typedef ptr Graph[MAX];
 typedef int vertex;
@@ -20,21 +26,86 @@ ptr createNode(int value);
 void insertLast(ptr *N, int value);
 void display(Graph G);
 
+void dfs(Graph G, vertex V, int mark[]);
+void bfs(Graph G, vertex V, int visited[], Queue *Q);
+void initQ(Queue *Q);
+void enqueue(Queue* Q, int value);
+void dequeue(Queue* Q);
+
 int main(){
     Graph G;
+    int visited[MAX];
     
     insertLast(&G[0], 1);
     insertLast(&G[0], 2);
-    insertLast(&G[0], 3);
+    insertLast(&G[1], 0);
     insertLast(&G[1], 2);
+    insertLast(&G[2], 1);
+    insertLast(&G[2], 0);
+    insertLast(&G[2], 3);
+    insertLast(&G[2], 4);
+    insertLast(&G[3], 2);
+    insertLast(&G[4], 2);
+    
+    //dfs(G, 2, visited);
+    
+    Queue Q;
+    
+    bfs(G, 0, visited, &Q);
     
     display(G);
+    
     
     
     return 0;
 }
 
 
+void initQ(Queue *Q){
+    Q->front = 0;
+    Q->rear = 0;
+}
+void enqueue(Queue* Q, int value){
+    Q->q[Q->rear++] = value;
+}
+void dequeue(Queue* Q){
+    Q->front++;
+}
+
+// UNFINISHED
+void bfs(Graph G, vertex V, int visited[], Queue *Q){
+    if(visited[V] == VISITED){
+        dequeue(Q);
+    }
+    
+    visited[V] = VISITED;
+    printf("%d ", V);
+    
+    enqueue(Q, V);
+    
+    ptr trav = G[]
+    
+    
+    
+    
+}
+
+
+void dfs(Graph G, vertex V, int mark[]){
+    mark[V] = VISITED;
+    printf("%d ", V);
+    
+    ptr trav = G[V];
+    int newV;
+    
+    while(trav != NULL){
+        newV = trav->elem;
+        if(mark[newV] == UNVISITED){
+            dfs(G, newV, mark);
+        }
+        trav = trav->link;
+    }
+}
 
 ptr createNode(int value){
     ptr newNode = malloc(sizeof(Node));
@@ -43,6 +114,8 @@ ptr createNode(int value){
     
     return newNode;
 }
+
+
 void insertLast(ptr *N, int value){
     ptr newNode = createNode(value);
     
@@ -59,6 +132,8 @@ void insertLast(ptr *N, int value){
         
     }
 }
+
+
 void display(Graph G){
     ptr trav;
     printf("\n----DISPLAY----\n");
